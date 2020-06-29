6fb41255dd5dec2bc3d48bda9d6e1975f6dbe849
kevin.zou <kevin.zou@makeblock.com>
2020-06-29 19:29:24 +0800

    feat: 更改为前端仓库

--- app/services/common/common.go
+++
@@ -1,6 +0,0 @@
-package common
-
-func SliceInsert(s []int, index int, value int) []int {
-	rear := append([]int{}, s[index:]...)
-	return append(append(s[:index], value), rear...)
-}
--- doc/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# match-schedule
-
-## v1.0.0
-
-- xxx
